be1a7d5be8fc33b8ac6c7ab2204141caa2e9781c
Rob Haswell <me@robhaswell.co.uk>
2023-05-06 12:35:43 +0100

    Solid colour.

--- colours.py
+++ colours.py
@@ -3,14 +3,27 @@ c = 0
 r = 0
 i = 0
 
+print("# led")
+
 for n in range(24):
-    print(f"led {n} {c},{r}::CT{i}")
+    print(f"led {n} {c},{r}::C:{i}")
     c += 1
     if c > 15:
         c = 0
         r += 1
-    if n >= 3:
+    if n >= 4:
         i += 1
         if i > 15:
             i = 15
-            
+
+print()
+print("# color")
+
+h = 0
+n = 0
+
+while n < 16:
+    hr = round(h)
+    print(f"color {n} {hr},0,255")
+    n += 1
+    h += 20
